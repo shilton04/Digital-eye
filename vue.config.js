e5ec7adcd8929f8cbdb670ace85d393c45106185
shilton04 <shiltonr42@gmail.com>
2024-10-14 13:27:30 +0530

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/Digital-eye/" // Replace <repository-name> with your actual repo name
+      : "/",
+});
